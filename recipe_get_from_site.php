1ea4b3246e7ec55e436cd564edde2b929be2e02b
TOGEKICHI-NOTE\yoshi <togekichi@mineo.jp>
2018-06-04 23:13:01 +0900

    no message

--- recipe_get_from_site.php
+++ recipe_get_from_site.php
@@ -18,6 +18,12 @@
       padding: 4px 4px 4px 4px;
       font-size: smaller;
     }
+    .border_inside {
+      border: #cccccc 1px solid;
+      border-collapse: collapse;
+      padding: 4px 4px 4px 4px;
+      font-size: smaller;
+    }
     </style>
   </head>
   <body>
@@ -31,10 +37,25 @@ $array = array_filter($array, 'strlen'); // 文字数が0の行を取り除く
 $array = array_values($array); // これはキーを連番に振りなおしてるだけ
 
 foreach ($array as $line) {
-  preg_match('/<strong class=\'name\'>[^<]+<\/strong>/', $line, $match);
-  if (strlen($match[0]) > 0) {
+  $match_result = "";
+  preg_match('/<strong class=\'name\'>[^<]+<\/strong>/', $line, $match1);
+  if (strlen($match1[0]) <= 0) {
+    preg_match('/^(DFS[^<]+)<br/>/', $line, $match2);
+    if (strlen($match2[1]) <= 0) {
+      preg_match('/^([0-9] \-[^<]+)<br/>/', $line, $match3);
+      if (strlen($match2[1]) <= 0) {
+      } else {
+        $match_result = $match3[1];
+      }
+    } else {
+      $match_result = $match2[1];
+    }
+  } else {
+    $match_result = $match1[0];
+  }
+  if (strlen($match_result) > 0) {
 ?>
-  <p>■<?php print($match[0]); ?>■</p>
+  <div class="border_inside"><?php print($match_result); ?></div>
 <?php
   }
 }
