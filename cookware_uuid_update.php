aa3e9a521ba05f8baf9d4dbdc9625e5eea45ea11
TOGEKICHI-NOTE\yoshi <togekichi@mineo.jp>
2018-06-17 02:06:53 +0900

    no message

---
+++ cookware_uuid_update.php
@@ -0,0 +1,84 @@
+<html>
+  <head>
+    <title>DFS Foodstuff Input Form</title>
+  </head>
+  <body>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+$conn = "host=ec2-23-23-248-192.compute-1.amazonaws.com dbname=dl8app8ukml19 user=zukuhaourmbbsk password=f9e66d533b3f6cdae3d67c88e7baac7bc05f380fcaf047471e726d3b332ef74a";
+$link = pg_connect($conn);
+if (!$link) {
+  die('接続失敗です。'.pg_last_error());
+}
+// 接続に成功
+
+// フォーム内容取得
+$dst_name = $_POST['DST_NAME'];
+$icon_uuid = $_POST['ICON_UUID'];
+$pict_uuid = $_POST['PICT_UUID'];
+$mode = $_POST['MODE'];
+
+if (intVal($mode) == 1) {
+  // 新規登録
+  $result = pg_query('
+INSERT INTO
+  dfs_uuid_join
+    (dst_name, icon_uuid, pict_uuid, update_date, regist_date)
+  VALUES
+    (\'' . $dst_name . '\', \'' . $icon_uuid . '\', \'' . $pict_uuid . '\', current_timestamp, current_timestamp)
+');
+  if (!$result) {
+    die('クエリーが失敗しました。'.pg_last_error());
+  }
+} else if (intVal($mode) == 2) {
+  // 更新
+  $result = pg_query('
+UPDATE
+  dfs_uuid_join
+SET
+  icon_uuid = \'' . $icon_uuid . '\',
+  pict_uuid = \'' . $pict_uuid . '\',
+  update_date = current_timestamp
+WHERE
+  dst_name = \'' . $dst_name . '\'
+');
+  if (!$result) {
+    die('クエリーが失敗しました。'.pg_last_error());
+  }
+} else if (intVal($mode) == 9) {
+  // 削除
+  $result = pg_query('
+DELETE
+FROM
+  dfs_uuid_join
+WHERE
+  dst_name = \'' . $dst_name . '\'
+  AND
+  icon_uuid = \'' . $icon_uuid . '\'
+  AND
+  pict_uuid = \'' . $pict_uuid . '\'
+');
+  if (!$result) {
+    die('クエリーが失敗しました。'.pg_last_error());
+  }
+}
+?>
+    <h3>DFS 調理器具 UUID 登録フォーム 処理完了</h3>
+      <h4>対象素材</h4>
+      <?php print($dst_name); ?><br/>
+      <br/>
+      アイコンUUID：<br/>
+      <?php print($icon_uuid); ?><br/>
+      写真UUID：<br/>
+      <?php print($pict_uuid); ?><br/>
+      <br/>
+      <br/>
+      <a href="./cookware_uuid_form.php">フォームへ戻る</a>
+<?php
+  $close_flag = pg_close($link);
+  if ($close_flag){
+    //     print('切断に成功しました。<br>');
+  }
+?>
+  </body>
+</html>
